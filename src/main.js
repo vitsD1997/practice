95146211976ff902166c4d1b859e2006786cad9a
vitthal <dhakanevitthal30@gmail.com>
2023-06-11 15:19:20 +0530

    done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import {} from "node:fs";
+import { MongoClient } from "mongodb";
 
-console.log("hello");
+ async function main() {
+    const uri = "mongodb://127.0.0.1";
+    const client = new MongoClient(uri);
+      //open the connection
+    const db = client.db("mydb");
+    const message = db.collection("message");
+    //close the connection
+   await message.insertOne({});
+   await client.close();
+   console.log("record added");
+}
+
+main();
