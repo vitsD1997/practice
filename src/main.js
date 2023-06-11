40b22e2d60f1d264d2b01aa0517fe0d6bd63f6ad
vitthal <dhakanevitthal30@gmail.com>
2023-06-11 17:36:03 +0530

    dine

--- src/main.js
+++ src/main.js
@@ -1,20 +1,41 @@
 import { MongoClient } from "mongodb";
 
-async function readMessage() {
-  const uri = "mongodb://127.0.0.1";
+async function insertMessage(jsonDocument) {
+  const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
-  //logic of read
+  const db = client.db("mydb");
+  const message = db.collection("message");
+
+  await message.insertOne(jsonDocument);
+
+  await client.close();
+  console.log("Record Added");
+}
+
+async function readMessage() {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  // logic of read
   let db = client.db("mydb");
   let message = db.collection("message");
 
   let list = await message.find().toArray();
-  console.log(list);
   await client.close();
+
+  return list;
 }
+
 async function main() {
-  let jsonDocument = { message: "hello world", to: "vitthal", from: "tejas" };
-  readMessage();
+  let jsonDocument = {
+    message: "Hello Universee",
+    to: "santosh",
+    from: "tejas",
+  };
+  // await insertMessage(jsonDocument);
+  let list = await readMessage();
+  console.log(list);
 }
 
 main();
