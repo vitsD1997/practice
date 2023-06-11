aa4caa262f85801bdc96371e07bf3c3d1d0afefd
vitthal <dhakanevitthal30@gmail.com>
2023-06-11 16:03:00 +0530

    data as parameter

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 import { MongoClient } from "mongodb";
 
- async function main() {
+ async function insertRecord() {
     const uri = "mongodb://127.0.0.1";
+    
     const client = new MongoClient(uri);
-      //open the connection
+
     const db = client.db("mydb");
     const message = db.collection("message");
+
+    let jsonDocument = { message: "hello world", to: "santosh", from: "tejas" };
+    await message.insertOne(jsonDocument);
+
     //close the connection
-   await message.insertOne({});
+
    await client.close();
    console.log("record added");
-}
+ }
+   async function main() {
+    let jsonDocument = { message: "hello world", to: "santosh", from: "tejas"};
+    await insertRecord(jsonDocument);
+   }
+
 
 main();
